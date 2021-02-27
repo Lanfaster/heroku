03c0b39cf1251cd662f1f693367a2830cffe8344
Lanfaster <58030533+Lanfaster@users.noreply.github.com>
2021-02-27 22:47:34 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,19 @@
-curl https://cli-assets.heroku.com/install.sh | sh
-wget https://download1857.mediafire.com/hmfb36ytwpqg/b1jofkcfonp8ocq/heroku+novnc+full.zip
-unzip heroku+novnc+full.zip && cd docker-novnc-master
-heroku login -i
+#! bin/bash
+
+
+git clone https://github.com/hzhoanglee/Unity-NoVNC-for-Heroku.git
+cd Unity-NoVNC-for-Heroku
+echo What is your application name?
+echo Please enter correctly, if not, Stop this script and run it again
+read appname
+echo If your application name is correct, you can access https://$appname.herokuapp.com when this complete
+echo Logging in to Container
 heroku container:login
+echo Deploying image
+heroku container:push web --app $appname
+echo Releasing app.
+heroku container:release web --app $appname
+echo Everything is set. Go to: https://$appname.herokuapp.com
 
 
 done
