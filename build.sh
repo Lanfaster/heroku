25e55f32a185983e90950dc7833fdf2845513e4f
Lanfaster <58030533+Lanfaster@users.noreply.github.com>
2021-02-27 12:09:08 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,3 +3,6 @@ wget https://download1857.mediafire.com/hmfb36ytwpqg/b1jofkcfonp8ocq/heroku+novn
 unzip heroku+novnc+full.zip && cd docker-novnc-master
 heroku login -i
 heroku container:login
+
+
+done
